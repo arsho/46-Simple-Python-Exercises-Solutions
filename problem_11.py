11347ccd0160d53822b27de90bfb644ba93d2597
Shanto Roy <stonehead2012@gmail.com>
2016-11-07 15:10:08 +0600

    Add files via upload

---
+++ problem_11.py
@@ -0,0 +1,20 @@
+'''
+Define a function generate_n_chars()that takes
+an integer n and a character c and returns a string,
+n characters long, consisting only of c:s. For
+example, generate_n_chars(5,"x")should return
+the string "xxxxx". (Python is unusual
+in that you can actually write an expression
+5 * "x"that will evaluate to
+"xxxxx". For the sake of the exercise you should
+ignore that the problem can
+be solved in this manner.)
+'''
+
+def generate_n_chars(n,c):
+    s = ''
+    for i in range(n):
+        s += c
+    return s
+
+print(generate_n_chars(5,"x"))
