215df94104c44135998e69609c6068f348b1a105
troyi <ratulbasak93@gmail.com>
2016-11-11 20:56:42 +0600

    Update problem_33.py

--- problem_33.py
+++ problem_33.py
@@ -1,4 +1,9 @@
-# "word" is a list and prints it in reverse or palindrome
+'''
+Statement:
+======================
+Define a word list that has two index. A variable is_palindrome takes the elements of the list and reverse 
+each elements by using [::-1] and the join method turns the list into desired output: "stressed desserts"
+'''
 
 word = ['stressed','desserts']
 is_palindrome = " ".join(word[::-1])
