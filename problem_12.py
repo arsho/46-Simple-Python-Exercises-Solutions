8d072dce77b4f22e584186edab20897fa851d8fe
Ahmedur Rahman Shovon <shovon.sylhet@gmail.com>
2019-09-27 19:09:30 +0600

    added 12

--- problem_12.py
+++ problem_12.py
@@ -7,10 +7,37 @@ For example, histogram([4, 9, 7])should print the following:
 *********
 *******
 '''
+import unittest
+
 
 def histogram(list_var):
+    s = ""
     for i in list_var:
-        print('*'*i)
+        s += '*' * i + "\n"
+    # remove the last new line
+    return s[:-1]
 
-histogram([4, 9, 7])
-    
+
+def histogram_alternative(list_var):
+    s = '\n'.join(['*' * i for i in list_var])
+    return s
+
+
+class TestHistogram(unittest.TestCase):
+    def setUp(self):
+        self.input_list = [4, 9, 7]
+        self.expected_solution = "****\n" + \
+                                 "*********\n" + \
+                                 "*******"
+
+    def test_histogram(self):
+        self.assertEqual(histogram(self.input_list),
+                         self.expected_solution)
+
+    def test_histogram_alternative(self):
+        self.assertEqual(histogram_alternative(self.input_list),
+                         self.expected_solution)
+
+
+if __name__ == "__main__":
+    unittest.main()
