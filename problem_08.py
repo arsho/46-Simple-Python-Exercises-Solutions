ff0712e7989fdb1412cab9fc7be0f59bf2fd013f
Ahmedur Rahman Shovon <shovon.sylhet@gmail.com>
2019-09-27 18:37:00 +0600

    added problem 8 and 9

--- problem_08.py
+++ problem_08.py
@@ -5,17 +5,34 @@ backwards). For example, is_palindrome("radar")
 should return True
 '''
 
-def is_palindrome(string):
-    ab = ''
-    length = len(string)
-    for i in range(length):
-        ab += string[length-1]
-        length -= 1
-    if string == ab:
-        return True
-    else:
-        return False
+import unittest
 
 
-print(is_palindrome("radar"))
-print(is_palindrome("abcde"))
+def is_palindrome(line):
+    reverse_line = ''
+    line_length = len(line)
+    for i in range(line_length - 1, -1, -1):
+        reverse_line += line[i]
+    return line == reverse_line
+
+
+def is_palindrome_short(line):
+    return line == line[::-1]
+
+
+# Below is the tests of the problem. Use the test cases to test your solution
+
+class TestIsPalindrome(unittest.TestCase):
+    def test_is_palindrome(self):
+        self.assertEqual(is_palindrome("radar"), True)
+        self.assertEqual(is_palindrome("abcde"), False)
+        self.assertEqual(is_palindrome(""), True)
+
+    def test_is_palindrome_short(self):
+        self.assertEqual(is_palindrome_short("radar"), True)
+        self.assertEqual(is_palindrome_short("abcde"), False)
+        self.assertEqual(is_palindrome_short(""), True)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- problem_09.py
+++ problem_09.py
@@ -7,18 +7,23 @@ the in operator does, but for the sake of the
 exercise you should pretend Python did not
 have this operator.)
 '''
+import unittest
 
-def is_member(item,list_var):
-    length = len(list_var)
-    i=0
-    while i < length:
-        if item == list_var[i-1]:
+
+def is_member(item, items):
+    items_length = len(items)
+    for i in range(items_length):
+        if item == items[i]:
             return True
-        i += 1
     return False
 
-abc = [1,4,6,9,34]
-print(is_member(9,abc))
-print(is_member(11,abc))
-    
 
+class TestIsMember(unittest.TestCase):
+    def test_is_member(self):
+        abc = [1, 4, 6, 9, 34]
+        self.assertEqual(is_member(9, abc), True)
+        self.assertEqual(is_member(11, abc), False)
+
+
+if __name__ == "__main__":
+    unittest.main()
