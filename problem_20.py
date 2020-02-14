c4de9c91f8a310419b723bc836c4fc4b36e9d215
Ahmedur Rahman Shovon <shovon.sylhet@gmail.com>
2020-02-14 19:38:23 +0600

    added 20

--- problem_20.py
+++ problem_20.py
@@ -7,17 +7,29 @@ English into Swedish. That is, write a function
 translate()that takes a list of
 English words and returns a list of Swedish words
 '''
+import unittest
 
-dic = {"merry":"god", "christmas":"jul",\
-"and":"och", "happy":"gott", "new":"nytt", "year":"år"}
+word_map = {"merry": "god", "christmas": "jul", \
+            "and": "och", "happy": "gott", "new": "nytt", "year": "år"}
 
-def translate(list_eng):
-    dic = {"merry":"god", "christmas":"jul",\
-"and":"och", "happy":"gott", "new":"nytt", "year":"år"}
-    list_dic = []
-    for i in list_eng:
-        list_dic.append(dic[i])
-    return list_dic
 
-ab = ["merry","happy"]
-print(translate(ab))
+def translate_to_swedish(english_words):
+    return [word_map.get(k) if word_map.get(k) else k for k in english_words]
+
+
+class TestTranslate(unittest.TestCase):
+    def test_get_song_99_bottles_of_beer(self):
+        expected_words = ["god", "jul", "dear"]
+        english_words = ["merry", "christmas", "dear"]
+        translated_words = translate_to_swedish(english_words)
+        number_of_lines = len(translated_words)
+        self.assertEqual(
+            number_of_lines, 3
+        )
+        self.assertEqual(
+            translated_words, expected_words
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
